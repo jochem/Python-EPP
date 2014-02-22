564ac5589b455285d445d4e1c11ccb147b15b18e
Jochem Oosterveen <jochem@oosterveen.net>
2014-02-22 14:54:11 +0100

    updated setup file

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     packages=['EPP'],
     description='Python EPP client',
     long_description=(
-        "Python-EPP provides an interface to the Extensible Provisioning"
-        "Protocol (EPP), which is being used for communication between domain"
+        "Python-EPP provides an interface to the Extensible Provisioning "
+        "Protocol (EPP), which is being used for communication between domain "
         "name registries and domain name registrars."
     ),
     install_requires=[
